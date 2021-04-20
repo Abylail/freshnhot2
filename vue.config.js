938833702c8c18ea36a1640b3bb461d117613d29
Abylail <bazarbayev.abylay@gmail.com>
2021-04-20 16:28:39 +0600

    prerender

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const PrerenderSpaPlugin = require("prerender-spa-plugin");
+var path = require('path');
+
 module.exports = {
   chainWebpack: config => {
       config
@@ -5,9 +8,23 @@ module.exports = {
           .tap(args => {
               args[0].title = "Freshnhot";
               return args;
-          })
+          });
   },
   devServer: {
     proxy: 'https://api.freshnhot.kz/'
+  },
+  configureWebpack: config => {
+      if (process.env.NODE_ENV !== 'production') return
+
+      return {
+          plugins: [
+              new PrerenderSpaPlugin(
+                  // Absolute path to compiled SPA
+                  path.resolve(__dirname, 'dist'),
+                  // List of routes to prerender
+                  [ '/', '/about'],
+              ),
+          ]
+      }
   }
 }
